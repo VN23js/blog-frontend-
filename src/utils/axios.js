f588ca0716cddaea1e551e9afe99e92edd5e020d
Ivan <ivannikolae2000@gmail.com>
2024-05-27 19:33:07 +0300

    REACT_APP_API_URL

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://blog-server-vn23js-projects.vercel.app/api",
+  baseURL: process.env.REACT_APP_API_URL,
   validateStatus: () => true
 });
 
